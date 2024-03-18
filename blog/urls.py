a0f27ca1929309e83c30f917423fde2fbf96fbb7
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-03-18 10:35:53 +0200

    Porfolio_simple

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+# urls.py
+from django.urls import path
+from .views import render_posts
+
+urlpatterns = [
+    path('', render_posts, name='posts'), 
+]
+
---
+++ blog/views.py
@@ -0,0 +1,5 @@
+# views.py
+from django.shortcuts import render
+
+def render_posts(request):
+    return render(request, 'posts.html')
