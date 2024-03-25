9c6019e9dde3633cab4e04819f6458e22e725941
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-03-25 07:33:53 +0200

    Otetaan käyttöön url posts.html, jossa urls.py, views.py ja db.sqlite on päivitetty

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
-# urls.py
+#urls.py 
 from django.urls import path
-from .views import render_posts
+from .views import render_posts, post_detail
 
 urlpatterns = [
-    path('', render_posts, name='posts'), 
+    path('', render_posts, name='posts'),
+    path('<int:post_id>/', post_detail, name='post_detail'),  
 ]
+
+
 
